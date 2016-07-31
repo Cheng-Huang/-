664e3899a9320cc9a7b4facb3e44a59d6dfa515d
黄成 <387241256@qq.com>
2016-07-31 21:27:56 +0800

    封装frame

---
+++ 百思不得姐/UIView+HCExtension.h
@@ -0,0 +1,18 @@
+//
+//  UIView+HCExtension.h
+//  百思不得姐
+//
+//  Created by 成 黄 on 16/7/31.
+//  Copyright © 2016年 成 黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (HCExtension)
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+
+@end
---
+++ 百思不得姐/UIView+HCExtension.m
@@ -0,0 +1,59 @@
+//
+//  UIView+HCExtension.m
+//  百思不得姐
+//
+//  Created by 成 黄 on 16/7/31.
+//  Copyright © 2016年 成 黄. All rights reserved.
+//
+
+#import "UIView+HCExtension.h"
+
+@implementation UIView (HCExtension)
+- (void)setWidth:(CGFloat)width
+{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (void)setX:(CGFloat)x
+{
+    CGRect frame = self.frame;
+    frame.origin.x = x;
+    self.frame = frame;
+}
+
+- (void)setY:(CGFloat)y
+{
+    CGRect frame = self.frame;
+    frame.origin.y = y;
+    self.frame = frame;
+}
+
+- (CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+- (CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+- (CGFloat)x
+{
+    return self.frame.origin.x;
+}
+
+- (CGFloat)y
+{
+    return self.frame.origin.y;
+}
+@end
