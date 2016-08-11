c367285c7ce69cec320b4d8b815351579cfacd35
黄成 <387241256@qq.com>
2016-08-11 21:37:11 +0800

    简易实现段子视图

--- 百思不得姐/Classes/Essence-精华/Controller/HCWordViewController.m
+++ 百思不得姐/Classes/Essence-精华/Controller/HCWordViewController.m
@@ -8,8 +8,13 @@
 //
 
 #import "HCWordViewController.h"
+#import <AFNetworking.h>
+#import <UIImageView+WebCache.h>
 
 @interface HCWordViewController ()
+
+/** 帖子数据 */
+@property (strong, nonatomic) NSArray *topics;
 
 @end
 
@@ -18,11 +23,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"]= @"list";
+    params[@"c"]= @"data";
+    params[@"type"]= @"29";
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        HCLog(@"success-----%@",responseObject);
+        self.topics = responseObject[@"list"];
+        [self.tableView reloadData];
+        HCLog(@"%@", self.topics);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        HCLog(@"failure----%@", error);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,25 +45,29 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.topics.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    static NSString *ID = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    
+    NSDictionary *topic = self.topics[indexPath.row];
+    cell.textLabel.text = topic[@"name"];
+    cell.detailTextLabel.text = topic[@"text"];
+    [cell.imageView sd_setImageWithURL:topic[@"profile_image"] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
